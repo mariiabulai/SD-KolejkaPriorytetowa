41c5518071ee9f8384d8a4769083be98453df6a2
Mariia <282655@student.pwr.edu.pl>
2025-05-07 11:48:40 +0200

    Update tablica.cpp

--- tablica.cpp
+++ tablica.cpp
@@ -1,55 +1,88 @@
 #include "tablica.h"
-#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-Tablica::Tablica() : counter(0) {}
+Tablica::Tablica(int initialCapacity) {
+    data = new Element[initialCapacity];
+    capacity = initialCapacity;
+    size = 0;
+    counter = 0;
+}
+
+Tablica::~Tablica() {
+    delete[] data;
+}
 
 void Tablica::insert(int value, int priority) {
-    data.push_back({ value, priority, counter++ });
+    if (size == capacity)
+        resize();
+
+    data[size].value = value;
+    data[size].priority = priority;
+    data[size].insertion_order = counter++;
+    size++;
 }
 
 int Tablica::extract_max() {
-    if (data.empty()) return INT_MIN; //Sprawdzenie, jeśli tablica pusta, zostaje zwrócona minimalną wartość int
+    if (size == 0) return INT_MIN; //Sprawdzenie, jeśli tablica pusta, zostaje zwrócona minimalną wartość int
 
     //element o najwyższym priorytecie (jeśli kilka, to najwcześniej dodany)
-    auto it = max_element(data.begin(), data.end(),
-        [](const Element& a, const Element& b) {
-            return a.priority < b.priority ||
-                (a.priority == b.priority && a.insertion_order > b.insertion_order);
-        });
-    int val = it->value;  //Zapisywanie wartości maksymalnego elementu
-    data.erase(it);       //usuwanie go z tablicy
-    return val;           //zwrócenie wartości
+    int max_index = 0;
+    for (int i = 1; i < size; ++i) {
+        if (data[i].priority > data[max_index].priority ||
+            (data[i].priority == data[max_index].priority &&
+                data[i].insertion_order < data[max_index].insertion_order)) {
+            max_index = i;
+        }
+    }
+
+    int val = data[max_index].value;
+    // Usuwanie elementu przez przesunięcie reszty
+    for (int i = max_index; i < size - 1; ++i) {
+        data[i] = data[i + 1];
+    }
+    size--;
+    return val;
 }
 
 //bez usuwania
 int Tablica::find_max() {
-    if (data.empty()) return INT_MIN;
-    auto it = max_element(data.begin(), data.end(),
-        [](const Element& a, const Element& b) {
-            return a.priority < b.priority ||
-                (a.priority == b.priority && a.insertion_order > b.insertion_order);
-        });
-    return it->value;
+    if (size == 0) return INT_MIN;
+    int max_index = 0;
+    for (int i = 1; i < size; ++i) {
+        if (data[i].priority > data[max_index].priority ||
+            (data[i].priority == data[max_index].priority &&
+                data[i].insertion_order < data[max_index].insertion_order)) {
+            max_index = i;
+        }
+    }
+    return data[max_index].value;
 }
 
 void Tablica::modify_key(int value, int new_priority) {
-    for (auto& el : data) {
-        if (el.value == value) {
-            el.priority = new_priority;
+    for (int i = 0; i < size; ++i) {
+        if (data[i].value == value) {
+            data[i].priority = new_priority;
             return;
         }
     }
 }
 
 int Tablica::return_size() const {
-    return data.size();
+    return size;
 }
 
 void Tablica::clear() {
-    data.clear();
-    data.reserve(100000);  //Rezerwuje miejsce na 100000 elementów, by uniknąć częstych alokacji
+    size = 0;
     counter = 0; //Resetuje licznik kolejności wstawiania
+}
+
+void Tablica::resize() {
+    capacity *= 2;
+    Element* newData = new Element[capacity];
+    for (int i = 0; i < size; ++i)
+        newData[i] = data[i];
+    delete[] data;
+    data = newData;
 }
