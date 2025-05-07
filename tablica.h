5cec8b405e22af2d06654d18505522b6b7f2f15b
Mariia <282655@student.pwr.edu.pl>
2025-05-07 11:48:27 +0200

    Update tablica.h

--- tablica.h
+++ tablica.h
@@ -1,26 +1,29 @@
 #pragma once
 
-#include <vector>
 #include <iostream>
 
 using namespace std;
 
 class Tablica {
-private:
+public:
     struct Element {
         int value;
         int priority;
         int insertion_order;
     };
-    vector<Element> data;
-    int counter;
-
-public:
-    Tablica();
+    Tablica(int initialCapacity = 16);
+    ~Tablica();
     void insert(int value, int priority);
     int extract_max();
     int find_max();
     void modify_key(int value, int new_priority);
-    int return_size() const;
     void clear();
+    void resize();
+    int return_size() const;
+
+private:
+    Element* data;
+    int capacity;
+    int size;
+    int counter;
 };
