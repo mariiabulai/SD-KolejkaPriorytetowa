28c0ba188b2165aaf94d4ffb4a50edaa434baf58
Mariia <282655@student.pwr.edu.pl>
2025-05-07 11:49:04 +0200

    Update kopiec.h

--- kopiec.h
+++ kopiec.h
@@ -1,11 +1,10 @@
 #pragma once
-#include <vector>
 #include <iostream>
 
 using namespace std;
 
 class Kopiec {
-private:
+public:
     struct Element {
         int value;
         int priority;
@@ -13,20 +12,22 @@ private:
         //Operator porównania: element z wyższym priorytetem lub wcześniejszym wstawieniem jest "większy"
         bool operator<(const Element& other) const;
     };
-    vector<Element> heap; //Wektor przechowujący elementy kopca
-    int counter;
-
-    void heapify_up(int index);
-    void heapify_down(int index);
-    int find_index(int value);
-
-public:
-    Kopiec();
+    Kopiec(int initialCapacity = 16);
+    ~Kopiec();
     void insert(int value, int priority);
     int extract_max();
     int find_max();
     void modify_key(int value, int new_priority);
     int return_size() const;
     void clear();
+    void resize();
+    int find_index(int value);
+private:
+    int capacity;
+    int size;
+    int counter;
+    Element* heap; //przechowuje elementy kopca
+    void heapify_up(int index);
+    void heapify_down(int index);
 };
 
