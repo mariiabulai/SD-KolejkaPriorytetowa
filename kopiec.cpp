34a6ad32f5d907b08c42dfce3d30a86f0aec7ec1
Mariia <282655@student.pwr.edu.pl>
2025-05-07 11:49:17 +0200

    Update kopiec.cpp

--- kopiec.cpp
+++ kopiec.cpp
@@ -1,5 +1,4 @@
 #include "kopiec.h"
-#include <algorithm>
 #include <iostream>
 
 //porównywanie priorytetów
@@ -8,24 +7,47 @@ bool Kopiec::Element::operator<(const Element& other) const {
         (priority == other.priority && insertion_order > other.insertion_order);
 }
 
-Kopiec::Kopiec() : counter(0) {}
+Kopiec::Kopiec(int initialCapacity) {
+    heap = new Element[initialCapacity];
+    capacity = initialCapacity;
+    size = 0;
+    counter = 0;
+}
+
+Kopiec::~Kopiec() {
+    delete[] heap;
+}
+
+void Kopiec::resize() {
+    capacity *= 2;
+    Element* newKopiec = new Element[capacity];
+    for (int i = 0; i < size; ++i)
+        newKopiec[i] = heap[i];
+    delete[] heap;
+    heap = newKopiec;
+}
 
 void Kopiec::insert(int value, int priority) {
-    heap.push_back({ value, priority, counter++ });
-    heapify_up(heap.size() - 1);
+    if (size == capacity)
+        resize();
+
+    heap[size] = { value, priority, counter++ };
+    heapify_up(size);
+    size++;
 }
 
 int Kopiec::extract_max() {
-    if (heap.empty()) return INT_MIN;
+    if (size == 0) return INT_MIN;
+
     int val = heap[0].value;
-    heap[0] = heap.back();
-    heap.pop_back();
+    heap[0] = heap[size - 1];
+    size--;
     heapify_down(0);
     return val;
 }
 
 int Kopiec::find_max() {
-    if (heap.empty()) return INT_MIN;
+    if (size == 0) return INT_MIN;
     return heap[0].value;
 }
 
@@ -35,7 +57,6 @@ void Kopiec::modify_key(int value, int new_priority) {
 
     int old_priority = heap[index].priority;
     heap[index].priority = new_priority;
-
     //W zależności od tego, czy priorytet wzrósł czy zmalał, trzeba wykonać odpowiedni heapify
     if (new_priority > old_priority)
         heapify_up(index);
@@ -43,12 +64,13 @@ void Kopiec::modify_key(int value, int new_priority) {
         heapify_down(index);
 }
 
+
 int Kopiec::return_size() const {
-    return heap.size();
+    return size;
 }
 
 void Kopiec::clear() {
-    heap.clear();
+    size = 0;
     counter = 0;
 }
 
@@ -56,13 +78,15 @@ void Kopiec::heapify_up(int i) {
     while (i > 0) {
         int parent = (i - 1) / 2;
         if (!(heap[parent] < heap[i])) break; //jeśli rodzic nie jest mniejszy, zakończyć
-        swap(heap[parent], heap[i]);     //zamienić miejscami z rodzicem
+        //zamienić miejscami z rodzicem
+        Element temp = heap[i];
+        heap[i] = heap[parent];
+        heap[parent] = temp;
         i = parent;
     }
 }
 
 void Kopiec::heapify_down(int i) {
-    int size = heap.size();
     while (true) {
         int left = 2 * i + 1, right = 2 * i + 2, largest = i;
 
@@ -73,13 +97,16 @@ void Kopiec::heapify_down(int i) {
         if (right < size && heap[largest] < heap[right]) largest = right;
 
         if (largest == i) break; //Jeśli największy to i, zakończenie
-        std::swap(heap[i], heap[largest]); //Zamienić miejscami z większym dzieckiem
+         //Zamienić miejscami z większym dzieckiem
+        Element temp = heap[i];
+        heap[i] = heap[largest];
+        heap[largest] = temp;
         i = largest; //Kontynuować dla nowej pozycji
     }
 }
 
 int Kopiec::find_index(int value) {
-    for (int i = 0; i < heap.size(); ++i)
+    for (int i = 0; i < size; ++i)
         if (heap[i].value == value) return i;
     return -1;
 }
